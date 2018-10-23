258cac8d12f1822c71f5ce2d9fadb22a88f0b0ba
StoWoW <43999446+StoWoW@users.noreply.github.com>
2018-10-23 17:32:51 -0500

    Add files via upload

---
+++ EJ15/EJ15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EJ15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            String cad = "";
+            for (int i = 0; i <= 100; i = i + 1)
+            {
+                cad = cad + " " + i;
+                sum = sum + i;
+            }
+            Console.WriteLine(cad);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EJ17/EJ17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EJ17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nro = 1;
+            for (nro = 1; nro < 101; nro++)
+                Console.Write(nro + "=" + Math.Pow(nro, 2) + "\n\n");
+            Console.ReadKey();
+        }
+    }
+}
