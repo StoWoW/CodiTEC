0dce4ed4e302093f261a0a922981ae2d3b4c3692
StoWoW <43999446+StoWoW@users.noreply.github.com>
2018-10-23 16:40:32 -0500

    Add files via upload

---
+++ 1 a 100 impares/ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {   
+                    Console.WriteLine("el numero es = "+ i);
+                        }
+            }
+        }
+    }
+}
+
